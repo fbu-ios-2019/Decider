e8180a3927336bf30aaf1b7f53d563f1bd5a3743
Katelin Chan <kchan23@fb.com>
2019-07-15 17:13:19 -0700

    Initial Commit

---
+++ Decider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Decider
+//
+//  Created by kchan23 on 7/15/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

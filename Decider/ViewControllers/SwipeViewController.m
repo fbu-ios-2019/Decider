5cbad8d5d8d91eae7b49752eb9cac4a9416fa4b5
Katelin Chan <kchan23@fb.com>
2019-07-17 10:39:36 -0700

    Separate Scroll View Controller added

---
+++ Decider/ViewControllers/SwipeViewController.m
@@ -0,0 +1,32 @@
+//
+//  SwipeViewController.m
+//  Decider
+//
+//  Created by kchan23 on 7/17/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import "SwipeViewController.h"
+
+@interface SwipeViewController ()
+
+@end
+
+@implementation SwipeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

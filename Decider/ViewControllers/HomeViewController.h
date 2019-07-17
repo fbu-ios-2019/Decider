1c45d93a5b967ae5360138b500147e60a7e92f30
Katelin Chan <kchan23@fb.com>
2019-07-17 10:14:35 -0700

    View Controller set up

---
+++ Decider/ViewControllers/HomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.h
+//  Decider
+//
+//  Created by kchan23 on 7/17/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

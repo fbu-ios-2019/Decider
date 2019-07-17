9d7d289f332d6005f882d2c327f64ba138503219
Katelin Chan <kchan23@fb.com>
2019-07-17 11:35:39 -0700

    Working on the swiping animation

--- Decider/ViewControllers/SwipeViewController.m
+++ Decider/ViewControllers/SwipeViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SwipeViewController.h"
+#import <MDCSwipeToChoose/MDCSwipeToChoose.h>
 
 @interface SwipeViewController ()
 
@@ -14,9 +15,54 @@
 
 @implementation SwipeViewController
 
+#pragma mark - Creating and Customizing a MDCSwipeToChooseView
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    MDCSwipeToChooseViewOptions *options = [MDCSwipeToChooseViewOptions new];
+    options.likedText = @"Keep";
+    options.likedColor = [UIColor blueColor];
+    options.nopeText = @"Delete";
+    options.onPan = ^(MDCPanState *state){
+        if (state.thresholdRatio == 1.f && state.direction == MDCSwipeDirectionLeft) {
+            NSLog(@"Let go now to delete the photo!");
+        }
+    };
+    
+    MDCSwipeToChooseView *view = [[MDCSwipeToChooseView alloc] initWithFrame:self.view.bounds
+                                                                     options:options];
+    view.imageView.image = [UIImage imageNamed:@"photo"];
+    [self.view addSubview:view];
+}
+
+#pragma mark - MDCSwipeToChooseDelegate Callbacks
+// User didn't fully swipe left or right.
+- (void)viewDidCancelSwipe:(UIView *)view {
+    NSLog(@"Couldn't decide, huh?");
+}
+
+// Sent before a choice is made. Cancel the choice by returning `NO`. Otherwise return `YES`.
+- (BOOL)view:(UIView *)view shouldBeChosenWithDirection:(MDCSwipeDirection)direction {
+    if (direction == MDCSwipeDirectionLeft) {
+        return YES;
+    } else {
+        // Snap the view back and cancel the choice.
+        [UIView animateWithDuration:0.16 animations:^{
+            view.transform = CGAffineTransformIdentity;
+            view.center = [view superview].center;
+        }];
+        return NO;
+    }
+}
+
+// User swipes the view fully left or right.
+- (void)view:(UIView *)view wasChosenWithDirection:(MDCSwipeDirection)direction {
+    if (direction == MDCSwipeDirectionLeft) {
+        NSLog(@"Photo deleted!");
+    } else {
+        NSLog(@"Photo saved!");
+    }
 }
 
 /*

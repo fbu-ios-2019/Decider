4fa74dba94b26aad5bb48b881d3727c00fc5ac03
María Alejandra Pestana Viso <marialepestana@fb.com>
2019-07-23 08:39:11 -0700

    Added RecommendationCell files and table view in storyboard

---
+++ Decider/Views/RecommendationCell.h
@@ -0,0 +1,17 @@
+//
+//  RecommendationCell.h
+//  Decider
+//
+//  Created by marialepestana on 7/23/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RecommendationCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Decider/Views/RecommendationCell.m
@@ -0,0 +1,24 @@
+//
+//  RecommendationCell.m
+//  Decider
+//
+//  Created by marialepestana on 7/23/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import "RecommendationCell.h"
+
+@implementation RecommendationCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
